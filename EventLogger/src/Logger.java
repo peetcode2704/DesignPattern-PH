515168d4d04b9d0e8975cfef0cd5fae9cb87edc8
Peter Hoang <peterhoang2704@gmail.com>
2024-11-26 20:43:05 -0600

    Implemented user story 5 that can saves the history log messages into a file called messages.log

--- EventLogger/src/Logger.java
+++ EventLogger/src/Logger.java
@@ -1,12 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Logger {
     private static Logger instance;
     private List<String> logHistory;
+    private BufferedWriter fileWriter;
 
     private Logger() {
         logHistory = new ArrayList<>();
+        try {
+            fileWriter = new BufferedWriter(new FileWriter("messages.log", true));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static Logger getInstance() {
@@ -20,11 +29,24 @@ public class Logger {
 
     private void log(String severityLevel, String message) {
         String logMessage = "[" + severityLevel + "] " + message;
+
+        // Write to console
         System.out.println(logMessage);
+
+        // Write to file
+        try {
+            if (fileWriter != null) {
+                fileWriter.write(logMessage);
+                fileWriter.newLine();
+                fileWriter.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         logHistory.add(logMessage);
     }
 
-    // Public methods for each log level
     public void info(String message) {
         log("INFO", message);
     }
@@ -39,5 +61,15 @@ public class Logger {
 
     public List<String> getLogHistory() {
         return logHistory;
+    }
+
+    public void close() {
+        try {
+            if (fileWriter != null) {
+                fileWriter.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
